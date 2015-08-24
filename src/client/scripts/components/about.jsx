09a14cf70694eb4f01475fcfcb0ebaefbe56fa6f
Mark Brownsword <markbrownsword@gmail.com>
2015-08-24 19:59:32 +1000

    initial commit

---
+++ src/client/scripts/components/about.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require("react");
+
+var About = React.createClass({
+    render: function() {
+        return (
+            <section>
+                <p>The Device Survey is a demo website built using React, Less, Webpack and Node.js</p>
+            </section>
+        );
+    }
+});
+
+module.exports = About;
